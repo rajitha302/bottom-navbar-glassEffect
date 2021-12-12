815650fc006998575a662c3dae9c8148026369b0
rajitha302 <rajithadisanayaka999@gmail.com>
2021-12-12 07:30:59 +0530

    1 file modified

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -6,15 +6,49 @@ color: #fff;
 `;
 
 const Card = styled.div`
+width: 50vw;
+height: 20vh;
+border-radius: 16px;
+border: 1px solid rgba(43, 43, 43, 0.568);
+position: relative;
+margin: 15px;
+padding: 15px;
+overflow: hidden;
+display: flex;
+justify-content: space-around;
+
+&:before {
+  position: absolute;
+  content: '';
+  background: inherit;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  box-shadow: inset 0 0 0 3000px rgba(150, 150, 150, 0.192);
+  filter: blur(80px);
+  border-radius: 15px;
+  margin: -20px;
+}
+`;
+
+const Wrapper = styled.div`
 padding: 15px
+`;
+
+const Flex = styled.div`
+display: flex;
 `;
 
 const Cards = () => {
   return (
-    <>
-      <SectionHeading>Recent</SectionHeading>
-      <Card></Card>
-    </>
+    <Wrapper>
+      <SectionHeading>Recent Updates</SectionHeading>
+      <Flex>
+        <Card></Card>
+        <Card></Card>
+      </Flex>
+    </Wrapper>
   );
 };
 
